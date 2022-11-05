149ed1c5fea4a8af2efa0ff6540193c3e22935e6
karannya <annyakar.91@gmail.com>
2022-11-05 10:49:02 +0200

    Add weather app

---
+++ src/components/Form.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import axios from "axios";
+
+import Weather from './Weather';
+
+function Form() {
+  const [inputdata, setInputData] = useState('')
+  const [cityInfo, setcityInfo] = useState([])
+
+  const api_Key = process.env.REACT_APP_API_KEY;
+  const api_Url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${api_Key}&q=${cityInfo}`;
+
+  const showData = async() => {
+    try {
+  const response= await axios.get(api_Url)
+      return (setInputData(response.data[0]),  
+      setcityInfo('') )
+    } catch(error){
+      console.log(error)
+      }  
+  }
+
+  const checkData = (event) => {
+    setcityInfo(event.target.value);
+  }
+
+  return (
+    <>
+      <div className='main-div'>
+        <div className='card-container'>
+          <div className='img-container'>
+            {inputdata === undefined
+              ? <>
+                <h2>Data not found</h2>
+                <button className='try-btn' onClick={() => setInputData('')}>try again</button>
+              </>
+              :
+              <>
+                <div className='card-header'>
+                  <input type='search' value={cityInfo} placeholder='Enter the name of a city'
+                    className='inputdatafield'
+                    onChange={checkData} />
+                  <button type='submit' onClick={showData} className='weather-btn'>Show</button>
+                </div>
+              </>
+            }
+            <div className='card-body'>
+              {inputdata && <Weather inputdata={inputdata} />}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Form
+
+
+
---
+++ src/components/Weather.js
@@ -0,0 +1,58 @@
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Weather({ inputdata }) {
+    const [data, setData] = useState([]);
+    const api_Key = process.env.REACT_APP_API_KEY;
+    const api_Url = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${inputdata.Key}?apikey=${api_Key}`
+    console.log(inputdata.Key)
+    
+    useEffect(() => {
+        async function getData() {
+            try {
+                const response = await axios.get(api_Url)
+                return (setData(response.data),
+                console.log(response.data))
+            } catch (err) {
+                console.log('Error occured when fetching data');
+            }
+        }
+        getData();
+    }, [api_Url, inputdata.Key]);
+
+
+    const timeC = data?.DailyForecasts && (new Date(data.DailyForecasts[0].Date)).toISOString().split('T')[1].split(':')[0]
+    const isDay = timeC > 6 && timeC < 20
+
+    return (
+        <div> {(
+            <h3 className='city'>
+                {inputdata.EnglishName} <sup className='country'>{inputdata.Country.ID}</sup>
+            </h3>)}
+            <h3 className="weather-details">max {Math.ceil((5 / 9) * ((data?.DailyForecasts && data.DailyForecasts[0].Temperature.Maximum.Value) - 32))}&deg;C
+                min {Math.ceil((5 / 9) * ((data?.DailyForecasts && data.DailyForecasts[0].Temperature.Minimum.Value) - 32))}&deg;C</h3>
+                {/* {timeC} */}
+               {(isDay===true)
+                ? <>
+                    {console.log('day')}
+                    <img className="img"
+                        src={data?.DailyForecasts && `./images/${data.DailyForecasts[0].Day.Icon}.png`} alt="Weather img" />
+                    <h3 className="IconPhrase">{data?.DailyForecasts && data.DailyForecasts[0].Day.IconPhrase}</h3>
+                </>
+                :
+                <>
+                    {console.log('night')}
+                    <img className="img"
+                        src={data?.DailyForecasts && `./images/${data.DailyForecasts[0].Night.Icon}.png`} alt="Weather img" />
+
+                    <h3 className="IconPhrase">{data?.DailyForecasts && data.DailyForecasts[0].Night.IconPhrase}</h3>
+                </>
+            }
+
+        </div>
+    )
+
+}
+
+export default Weather
